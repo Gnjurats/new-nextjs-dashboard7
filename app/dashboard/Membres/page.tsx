6e480e1895ac7f96486b9912dc685518f11a9d2d
Gnjurats <brandstattern@gmail.com>
2024-10-30 17:56:25 +0100

    Update page.tsx

--- app/dashboard/Membres/page.tsx
+++ app/dashboard/Membres/page.tsx
@@ -1,311 +1,243 @@
 export default function Page() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: &apos;20px&apos; }}>
       {/* Titre en gros et gras */}
-      <h1 style={{ fontSize: '36px', fontWeight: 'bold', marginBottom: '40px' }}>Membres</h1>
+      <h1 style={{ fontSize: &apos;36px&apos;, fontWeight: &apos;bold&apos;, marginBottom: &apos;40px&apos; }}>Membres</h1>
       
-      {/* Phrase d'introduction en taille moyenne */}
-      <p style={{ fontSize: '24px', marginBottom: '40px' }}>
+      {/* Phrase d&apos;introduction en taille moyenne */}
+      <p style={{ fontSize: &apos;24px&apos;, marginBottom: &apos;40px&apos; }}>
         Bienvenue sur la page des membres du Groupe des Arbitres Genevois.
       </p>
       
       {/* Liste des membres */}
-      <ul style={{ listStyleType: 'none', paddingLeft: '0' }}>
-        <li style={{ marginBottom: '20px' }}>
+      <ul style={{ listStyleType: &apos;none&apos;, paddingLeft: &apos;0&apos; }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Sakariya Ahmed<br />
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Haidar Al Mayyah<br />
           <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Bekir Alakus<br />
           <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Gabriel Alves<br />
           <em>Soutenu par son club, le FC Genève United</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Gonzalo Arancibia<br />
           <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Besim Arifi<br />
           <em>Soutenu par son club, le AS Charmilles</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Mahamadi Bagagnan<br />
           <em>Soutenu par son club, le FC Plan-les-Ouates</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           David Barrio<br />
           <em>Soutenu par son club, le Urania Genève Sport</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Saifeddine Bejaoui<br />
           <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Akin Belek<br />
           <em>Soutenu par son club, le Urania Genève Sport</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Teodoro Bleve<br />
           <em>Soutenu par son club, le FC Académico de Viseu GE</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Jacques André Boesiger<br />
-          <em>Membre d'honneur</em>
+          <em>Membre d&apos;honneur</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Rayen Boughanmi<br />
           <em>Soutenu par son club, le FC Plan-les-Ouates</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Nicholas Brandstätter<br />
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Aboubacar Camara<br />
           <em>Soutenu par son club, le FC Puplinge</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Andreas Camargo<br />
           <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Christian Chiesa<br />
           <em>Soutenu par son club, le FC Etoile-Laconnex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Pedro André Couto<br />
           <em>Comité</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Antonio D'Amato<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Antonio D&apos;Amato<br />
           <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Stéphane De Almeida<br />
           <em>Soutenu par son club, le FC Académico de Viseu GE</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Claude Detruche<br />
-          <em>Membre d'honneur</em>
+          <em>Membre d&apos;honneur</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Mohamadou Ciré Diallo<br />
           <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Daniel Diallo<br />
           <em>Soutenu par son club, le FC Genève United</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           André dos Santos<br />
           <em>Soutenu par son club, le FC Puplinge</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Hicham El Asraoui<br />
           <em>Soutenu par son club, le FC Stade Francais</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Mohamed El Mouss<br />
           <em>Soutenu par son club, le FC Satigny</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Tomas Estifanos<br />
           <em>Soutenu par son club, le Urania Genève Sport</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Malik Ezzrari<br />
           <em>Soutenu par son club, le FC Etoile-Laconnex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Victor Faria<br />
           <em>Soutenu par son club, le FC Plan-les-Ouates</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Joao Figueiredo<br />
           <em>Soutenu par son club, le FC Académico de Viseu GE</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Alejandro Filgueira<br />
           <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Florian Fraguas Sanz<br />
           <em>Soutenu par son club, le US Lecce Genève</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Henrique Freitas<br />
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Daniel Frick<br />
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Daniel Fernando Gonzalez<br />
           <em>Soutenu par son club, le FC Puplinge</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Freddy Alejandro Higuera<br />
           <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Roberto Jeker<br />
           <em>Soutenu par son club, le Urania Genève Sport</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Stefano Lecci<br />
           <em>Soutenu par son club, le US Lecce Genève</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Seif Litim<br />
           <em>Soutenu par son club, le FC Coheran</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Daniel Locci<br />
           <em>Soutenu par son club, le FC Satigny</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Jacques André Boesiger<br />
-          <em>Membre d'honneur</em>
+          <em>Membre d&apos;honneur</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
           Quentin Mabille<br />
           <em>Soutenu par son club, le FC Académico de Viseu GE</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Nicolas Magnin<br />
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Samir Maliqi<br />
-          <em>Soutenu par son club, le FC Plan-les-Ouates</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Vincenzo Mariani<br />
-          <em>Membre d'honneur</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Giovanni Matteucci<br />
-          <em>Soutenu par son club, le FC Aïre-le-Lignon et membre d'honneur</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Jaison Mulegwa<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Zaki Mohamed<br />
           <em>Soutenu par son club, le Urania Genève Sport</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Salvatore Musso<br />
-          <em>Soutenu par son club, le FC Onex et membre d'honneur</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Stéphane Neuhäusler<br />
-          <em>Comité</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Yvan Kelie Nguessan<br />
-          <em>Soutenu par son club, le FC Osas</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Dany Novoa<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Elizandre Monteiro<br />
           <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Marco Oliveira<br />
-          <em>Comité</em>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Hugo Moura<br />
+          <em>Soutenu par son club, le FC Stade Francais</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Yannick Panzera<br />
-          <em>Soutenu par son club, le FC Grand-Saconnex</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Walter Paolozza<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Ammar Moustafa<br />
           <em>Soutenu par son club, le FC Satigny</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Kevin Pinto<br />
-          <em>Soutenu par son club, le FC Académico de Viseu GE et comité</em>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Marwan Moussa<br />
+          <em>Soutenu par son club, le FC Genève United</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Islam Rahimi<br />
-          <em>Soutenu par son club, le AS Charmilles</em>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Ismail El Ouadi<br />
+          <em>Soutenu par son club, le FC Etoile-Laconnex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Isaias Daniel Ramirez Almache<br />
-          <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Jonatan David Ramirez Almache<br />
-          <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Etienne Roux<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Josselin Paquet<br />
           <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Taha Saadallah<br />
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Kévin Sabater<br />
-          <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Kevin Saccone<br />
-          <em>Comité</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Sadri Saïeb<br />
-          <em>Soutenu par son club, le FC Puplinge</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Diego Salamo<br />
-          <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Andjelko Simic<br />
-          <em>Soutenu par son club, le Urania Genève Sport</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Ilija Simic<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Tony Pecoraro<br />
           <em>Soutenu par son club, le FC Plan-les-Ouates</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Filipe Soares<br />
-          <em>Soutenu par son club, le FC Académico de Viseu GE</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Victor Sossa Camacho<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Simon Piteau<br />
           <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Serge Studli<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Habib Rahman<br />
           <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Gabriel Sugnaux<br />
-          <em>Membre d'honneur</em>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Pierre Rechsteiner<br />
+          <em>Soutenu par son club, le Urania Genève Sport</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Rachid Taouji<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Benin Saibou<br />
+          <em>Soutenu par son club, le FC Genève United</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Kazim Uysal<br />
-          <em>Comité</em>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Mohamed Sbiha<br />
+          <em>Soutenu par son club, le FC Stade Francais</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Miguel Vieira<br />
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Giacomo Venturi<br />
+          <em>Soutenu par son club, le FC Onex</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Nicolas Wilde<br />
-          <em>Soutenu par son club, le FC Collex-Bossy</em>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Antonino Vitale<br />
+          <em>Soutenu par son club, le FC Plan-les-Ouates</em>
         </li>
-        <li style={{ marginBottom: '20px' }}>
-          Zoumonzian Franck Ivan Zahon<br />
-          <em>Soutenu par son club, le FC Aïre-le-Lignon</em>
-        </li>
-        <li style={{ marginBottom: '20px' }}>
-          Robeto Zezza<br />
-          <em>Membre d'honneur</em>
+        <li style={{ marginBottom: &apos;20px&apos; }}>
+          Brayan Zamora<br />
+          <em>Soutenu par son club, le FC Genève United</em>
         </li>
       </ul>
     </div>
